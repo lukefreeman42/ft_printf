250f9dcc036fe3805fa5abf94ea7b3346fda68de
lukefreeman42 <lukefreeman@protonmail.com>
2019-03-19 13:17:11 -0700

    alpha functions done

--- alpha_functions.c
+++ alpha_functions.c
@@ -12,12 +12,28 @@ static void putpad(char pad[16], int a)
     write(1, pad, mod);
 }
 
-void    char_ph(char buff[65], va_list arg, t_flags flags)
+static void putbuff(char buff[65], char *str, t_flags flags, int len)
 {
-    char c;
+    int div;
+    int mod;
+    int n;
 
-    c = (char)va_arg(arg, int);
-    write(1, &c, 1);
+    n = flags.width > len ? len : flags.width;
+    div = flags.precision ? n / 64 : len / 64;
+    mod = flags.precision ? n % 64 : len % 64;
+    while (div-- && !(buff[64] = 0))
+    {
+        str += ft_strwcpy(buff, str, 64);
+        ft_putstr(buff);
+    }
+    if (flags.precision && !(buff[mod] = 0))
+        ft_strwcpy(buff, str, mod);
+    else
+    {
+        ft_strwcpy(buff, str, mod);
+        buff[mod] = 0;
+    }
+    ft_putstr(buff);
 }
 
 void    str_ph(char buff[65], va_list arg, t_flags flags)
@@ -27,20 +43,29 @@ void    str_ph(char buff[65], va_list arg, t_flags flags)
     int     len;
     char    *pad;
 
-    n = flags.width;
-    pad = flags.zero ? zeros : spaces;
     str = va_arg(arg, char*);
-    if (flags.precision && !(buff[n] = 0))
-        ft_strwcpy(buff, str, n);
-    else
-    {
-        len = ft_strlen(str);
-        ft_strwcpy(buff, str, len);
-        buff[len] = 0;
-        if (!flags.neg)
+    n = flags.width;
+    len = ft_strlen(str);
+    pad = flags.zero ? zeros : spaces;
+    if (!flags.neg && !flags.precision && n > len)
             putpad(pad, n - len);
-    }
-    ft_putstr(buff);
-        if (flags.neg && !flags.precision)
-            putpad(spaces, n - len);
+    putbuff(buff, str, flags, len);
+    if (flags.neg && !flags.precision && n > len)
+        putpad(spaces, n - len);
+}
+
+void    char_ph(char buff[65], va_list arg, t_flags flags)
+{
+    char c;
+    int n;
+    char *pad;
+
+    n = flags.width;
+    c = (char)va_arg(arg, int);
+    pad = flags.zero ? zeros : spaces;
+    if (!flags.neg && !flags.precision && n > 1)
+        putpad(pad, n - 1);
+    write(1, &c, 1);
+    if (flags.neg && !flags.precision && n > 1)
+        putpad(spaces, n - 1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int     main(int argc, char **argv)
     char *b;
 
     a = 'h';
-    b = "hello";
+    b = "hello this should be over 64 bytes we will make sure it is, this is the only way to test forsure about whatever happens to see if my printf works like the standard using buff and no malloc, quick ! speed ! fast ! faster! fastest !";
     if (argc == 4)
     {
         ft_printf(argv[1], a, b);
