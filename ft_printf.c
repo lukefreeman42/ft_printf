31e7ae26e0174cef0c85986a2ed0766963a21aa0
lukefreeman42 <lukefreeman@protonmail.com>
2019-03-22 16:25:32 -0700

    normed ft_prinft.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,15 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 16:24:11 by llelias           #+#    #+#             */
+/*   Updated: 2019/03/22 16:25:14 by llelias          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 static	int		putbuff(char buff[65], int i)
 {
 	buff[i] = 0;
-	write(1 ,buff, ft_strlen(buff));
+	write(1, buff, ft_strlen(buff));
 	return (0);
 }
 
-static	void	ft_prints(char *f, char buff[65], va_list arg)
+static	void	ft_prints(char *f, va_list arg)
 {
-	int i;
+	char	buff[65];
+	int		i;
 
 	i = 0;
 	while (*f)
@@ -20,21 +33,20 @@ static	void	ft_prints(char *f, char buff[65], va_list arg)
 			f = handle_ph(f, buff, arg);
 		}
 		else
-        {
-            if (i == 63)
-                i = putbuff(buff, 64);
-            buff[i++] = *f++;
-        }
-    }
-    putbuff(buff, i);
+		{
+			if (i == 63)
+				i = putbuff(buff, 64);
+			buff[i++] = *f++;
+		}
+	}
+	putbuff(buff, i);
 }
 
-int     ft_printf(char *f, ...)
+int				ft_printf(char *f, ...)
 {
-    char buff[65];
+	va_list	arg;
 
-    va_list arg;
-    va_start(arg, f);
-    ft_prints(f, buff, arg);
-    return (0);
+	va_start(arg, f);
+	ft_prints(f, arg);
+	return (0);
 }
