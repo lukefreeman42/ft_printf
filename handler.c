020a76e1fee0415582acfd54906bdba908545fd9
Luke Lelias <llelias@e1z2r9p10.42.us.org>
2019-04-14 17:47:31 -0700

    added L flag handler, norm forced bad code practice, but it is normed and finished

--- handler.c
+++ handler.c
@@ -40,31 +40,31 @@ static	void	fixflags(t_flags *op)
 
 static	char	*setflags(char *f, t_flags *op)
 {
-	if (*f == '.')
+	if (*f == '.' && f++)
 		op->precision += 1;
-	else if (is_num(*f) && *f != '0')
+	else if (is_num(*f) && *f != '0' && !op->width)
 	{
-		if (!op->width)
-			op->width = ft_atoi(f);
+		op->width = ft_atoi(f);
 		while (is_num(*f))
 			f++;
-		return (f);
 	}
-	else if (*f == '0')
+	else if (*f == 'L' && f++)
+		op->lfloat += 1;
+	else if (*f == '0' && f++)
 		op->zero += 1;
-	else if (*f == '-')
+	else if (*f == '-' && f++)
 		op->neg += 1;
-	else if (*f == '+')
+	else if (*f == '+' && f++)
 		op->add += 1;
-	else if (*f == ' ')
+	else if (*f == ' ' && f++)
 		op->space += 1;
-	else if (*f == 'l')
+	else if (*f == 'l' && f++)
 		op->l += 1;
-	else if (*f == 'h')
+	else if (*f == 'h' && f++)
 		op->h += 1;
-	else if (*f == '#')
+	else if (*f == '#' && f++)
 		op->alt += 1;
-	return (++f);
+	return (f);
 }
 
 char			*ph_handler(char *f, char b[65], va_list arg)
