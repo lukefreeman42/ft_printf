a6a42a044b120e75040019c80aef79230578f7c6
Luke Lelias <llelias@e1z2r9p10.42.us.org>
2019-04-17 18:48:51 -0700

    pudpad bug fixed

--- ph_alpha.c
+++ ph_alpha.c
@@ -16,12 +16,14 @@ static	void	putpad(char pad[16], int a)
 {
 	int	div;
 	int	mod;
-
-	div = a / 16;
-	mod = a % 16;
-	while (div--)
-		write(1, pad, 16);
-	write(1, pad, mod);
+	if (a > 0)
+	{
+		div = a / 16;
+		mod = a % 16;
+		while (div--)
+			write(1, pad, 16);
+		write(1, pad, mod);
+	}
 }
 
 static	void	evaluate_str(char buff[65], char *str, t_flags flags, int len)
